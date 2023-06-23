aae43ec722f0d457c71c78e61d3fd1cb50275d19
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2023-06-23 16:06:49 +0700

    untuk cek apakah antrian kososng

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -40,7 +40,13 @@ public:
 	}
 
 	void remove() {
-		
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queues underflow\n";
+			return;
+		}
+		cout << "\nThe element deleted from the queues is : " << queue_array[FRONT] << "\n";
 
+		
 		
 		
